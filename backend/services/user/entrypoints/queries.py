b9a2d227cc8b57628ea876349c492f28dec285e6
Coder1221 <21100328@lums.edu.pk>
2022-05-29 11:34:34 +0500

    WiP refactoring

--- backend/services/user/entrypoints/queries.py
+++ backend/services/user/entrypoints/queries.py
@@ -3,6 +3,8 @@ import services.exceptions as errors
 
 
 def user_login(user_obj: td.User, password: str) -> bool:
+    """Checks the user given password with stored password if both are same it will return True else it will raise error"""
     if user_obj.check_password(password):
         return True
     raise errors.LoginFailure
+ßßß
